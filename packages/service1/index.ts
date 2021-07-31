3986666dad5f9760c9d5b332838c94c093b0abea
Mikkel <mikkel@pearl.link>
2021-07-31 13:47:38 +1200

    Demonstrate tsconfig paths

--- packages/service1/index.ts
+++ packages/service1/index.ts
@@ -1,9 +1,19 @@
-const tsConfigPaths = require('tsconfig-paths') //eslint-disable-line
-const baseUrl = './' // Either absolute or relative path. If relative it's resolved to current working directory.
+const tsConfig = require("./tsconfig.json");
+const tsConfigPaths = require("tsconfig-paths");
+
+const baseUrl = "./"; // Either absolute or relative path. If relative it's resolved to current working directory.
 tsConfigPaths.register({
     baseUrl,
-    paths: {
-        '@/*': 'src/*'
-    },
-})
-export { func1 } from '@/func1'
+    paths: tsConfig.compilerOptions.paths
+});
+
+import { func1 } from '@/func1'
+
+function main () {
+    console.log(func1())
+}
+main()
+
+export {
+    func1
+}

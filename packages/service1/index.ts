608e9c1a404148c8f33d720f7dd801d1b4d2b655
Mikkel <mikkel@pearl.link>
2021-07-31 14:33:59 +1200

    Remove tsconfig and use ttsc transformer

--- packages/service1/index.ts
+++ packages/service1/index.ts
@@ -1,12 +1,3 @@
-const tsConfig = require("./tsconfig.json");
-const tsConfigPaths = require("tsconfig-paths");
-
-const baseUrl = "./"; // Either absolute or relative path. If relative it's resolved to current working directory.
-tsConfigPaths.register({
-    baseUrl,
-    paths: tsConfig.compilerOptions.paths
-});
-
 import { func1 } from '@/func1'
 
 function main () {
